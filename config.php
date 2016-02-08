81522ddebf9fb05f732326e642447637ce4a921e
Harikrishnan R <harikrishnanr994@gmail.com>
2016-02-08 12:26:18 +0530

    create config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', '	u830014551_d16');
+define('DB_PASSWORD', 'dvaita2016');
+define('DB_HOST', 'mysql.hostinger.in');
+define('DB_NAME', '	u830014551_d16');
+?>
