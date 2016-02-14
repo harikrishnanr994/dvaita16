dfe977cd75a1c3dd055cb9c3e96b5b5a4971fec5
Harikrishnan R <harikrishnanr994@gmail.com>
2016-02-14 09:50:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'dvaita16');
+define('DB_USERNAME', 'u830014551_d16');
+define('DB_PASSWORD', 'dvaita2016');
+define('DB_HOST', 'mysql.hostinger.in');
+define('DB_NAME', 'u830014551_d16');
 ?>
