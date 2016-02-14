98324bfd20b0f8fd3ccd008175f1ca503d1a9c17
harihara007 <harikrishnanr994@gmail.com>
2016-02-14 23:13:37 +0530

    python.php

---
+++ python.php
@@ -0,0 +1,159 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1" />
+
+    <title>Dvaita'16</title>
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/animations.css" />
+    <link rel="stylesheet" type="text/css" href="css/superslides.css" />
+    <link rel="stylesheet" type="text/css" href="css/flexslider.css" />
+    <link rel="stylesheet" type="text/css" href="css/masonry.css" />
+    <link rel="stylesheet" type="text/css" href="css/fancybox.css" />
+    <link rel="stylesheet" type="text/css" href="css/multicolor.css" />
+    <link rel="shortcut icon" href="img/favicon.png" />
+</head>
+<body style="background-color:#0d2f09;" >
+<div >
+
+
+    <!-- About Content -->
+    <div class="container">
+
+        <div style="color: #ffffff;font-size: 50px;margin: 35px 0;text-align: center;transition: all linear 0.25s;-ms-transition: all linear 0.25s;-moz-transition: all linear 0.25s;-webkit-transition: all linear 0.25s;-o-transition: all linear 0.25s;" class="text-center"><i class="icon-laptop" style=" margin-top: 0;padding: 20px;width: 100px;height: 100px;background: rgba(255,255,255,0.25);border-radius: 100%;font-size: 100px; line-height: 100px;color: #FFF;"></i><br><br><br><strong>WORKSHOP: ‘PRACTICAL PYTHON’</strong>
+            <p><span> </span></p>
+        </div>
+
+        <hr />
+
+
+            <img class="pic" src="img/Python.png"  alt="banner"  />
+
+            <br>
+
+            <h1 class="main-title"> <span>13 Feb 2016</span></h1>
+
+            <br>
+
+            <h1 class="main-title"><span>Report</span></h1>
+
+
+            <div class="row">
+                <div class="span6 offset3">
+                    <p class=" margin-bottom">A workshop on ‘Practical Python’ was held, on Saturday, 13th February 2016, as part of DVAITA’16, the National level technical fest organized by Department of  Computer Science & Engineering of NSS College of Engineering, Palakkad, India. Kerala, India,The workshop was conducted by Mr. Noufal Ibrahim, an expert trainer and member of Python Software Foundation.
+                        <br><br>The workshop began at 10 in the morning with about 80 students.The workshop was a lively one with regular interaction between the instructor and the students. It helped the students, gain an in-depth knowledge in python programming. There were also five student volunteers to help students during the workshop.<br><br>
+                        Mr.Ibrahim began the workshop by introducing a few warm up questions. Then the students approximated the value of PI using the python random library. Afterwards, they built a hangman style game and a chat server.<br><br>An orientation class was also conducted by student volunteers on Wednesday, 10th February to introduce the basic python syntax to students who were interested in participating in the workshop.<br><br>
+                        The workshop had support from the Python Software Foundation.
+                    </p>
+
+                </div>
+            </div>
+
+            <hr />
+
+            <h1 class="main-title"> <span> Gallery </span></h1>
+
+            <div class="masonry">
+
+                <!-- Portfolio Single item -->
+                <div ><img class="pic" src="img/img1.jpg" alt="Portfolio"  /></div>
+
+
+
+
+               <div> <img class="pic" src="img/img2.jpg" alt="Portfolio"  /></div>
+
+
+
+
+                <div ><img  class="pic" src="img/img3.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+
+                <div ><img  class="pic" src="img/img4.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+
+
+                <div ><img class="pic" src="img/img5.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+
+
+                <div ><img class="pic" src="img/img6.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+
+
+                <div ><img class="pic" src="img/img7.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+
+
+                <div ><img class="pic" src="img/img8.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+                <div ><img class="pic" src="img/img10.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+                <div ><img class="pic" src="img/img11.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+                <div ><img class="pic" src="img/img9.jpg" alt="Portfolio" class="thumb" /></div>
+
+
+            </div>
+
+
+
+
+
+
+
+            <hr /-->
+            <!-- Contact -->
+            <h1 class="main-title"> <span>Contact</span></h1>
+
+
+            <div class="row">
+                <div class="span6 offset3">
+
+                    <h4 style="color: #fff" align="center">Amal  - +91 828 136 0530  </h4>
+                    <h4 style="color: #fff" align="center">Shafeeq  - +91 808 965 2690  </h4>
+                    <br><br>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    <a href="www.dvaita16.com" class="section-close">×</a>
+    </div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/register.js"></script>
+<script type="text/javascript" src="js/login.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.animate-enhanced.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.superslides.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/jquery.masonry.min.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.js"></script>
+<script type="text/javascript" src="js/global.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+<script type="text/javascript" src="js/functions.js"></script>
+</body>
+</html>
